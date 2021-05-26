12fc5786d7823ff4f35eac49c71cdc005b87d147
Kevin De Notariis <kevin.denotariis@gmail.com>
2021-05-26 10:53:26 +0200

    First Commit

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -4,7 +4,9 @@ import got from "got";
 
 const routes = (app) => {
   app.get("/", (req, res) => {
-    return res.render("index.html", { title: "My App Developped in DevOps" });
+    return res.render("index.html", {
+      title: "My Web App With a CI / CD Pipeline",
+    });
   });
 
   app.get("/api/users", (req, res) => {
